b3f0fb8e3e436007ed11db621b0d01193a5519ed
nabilcambiaso <nabil.cambiaso@gmail.com>
2023-01-09 17:53:51 +0100

    Added flight api

--- api/controllers/dossier.js
+++ api/controllers/dossier.js
@@ -4,6 +4,7 @@ const knex = require('../../db');
 const client = require('./client');
 
 exports.addDossier = async (req, res, next) => {
+  console.log(req.body)
   try {
     const newClient = client.addClient(req, res);
     if (newClient) {
@@ -23,24 +24,21 @@ exports.addDossier = async (req, res, next) => {
           .returning('dossier_num')
           .then(async (dossier_num) => {
             await knex('flight').insert({
-              city_id_start: req.body.city_id_start,
+              city_id_start: 5,
+              // city_id_start: req.body.city_id_start,
               from_start: req.body.from_start,
               to_start: req.body.to_start,
               flight_start: req.body.flight_start,
               flight_time_start: req.body.flight_time_start,
               from_to_start: req.body.from_to_start,
-              dossier_id: dossier_num,
-              city_id_end: req.body.city_id,
+              dossier_id: dossier_num[0].dossier_num,
+              city_id_end: 5,
+              // city_id_end: req.body.city_id,
               flight_end: req.body.flight_end,
               from_end: req.body.from_end,
               to_end: req.body.to_end,
               from_to_end: req.body.from_to_end,
               flight_time_end: req.body.flight_time_end,   
-            }).then(() => {
-              return res.status(200).json({
-                success: true,
-                message: "Flight added successfully",
-              });
             });
             req.body.hotels_dossier.forEach(async (hotelForFolder) => {
               await knex('dossier_hotel')
