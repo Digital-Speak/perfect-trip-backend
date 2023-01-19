305f78732829c652a2ad08569deea26fe092c251
nabilcambiaso <nabil.cambiaso@gmail.com>
2023-01-19 11:46:17 +0100

    Add nodemailer and sent a mail

--- tools/mails/forgotPasswordMail.js
+++ tools/mails/forgotPasswordMail.js
@@ -2,28 +2,36 @@ require("dotenv").config();
 const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
+var nodemailer = require('nodemailer');
+
+
 
 module.exports = async function SendEmail(email, name, token) {
-  const msg = {
-    to: email,
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.MAINEMAIL,
+      pass: process.env.MAINEMAILPASS
+    }
+  });
+  
+  var mailOptions = {
     from: process.env.MAINEMAIL,
+    to: email,
     subject: 'Forgot password',
-    text: 'Change password',
-    html: `<div>
-    <strong>Hello ${name} Change your password</strong>
-    <strong>Token ${token}</strong>
-    <a href="${process.env.API_URL}/user/verifytoken/${token}">Change password</a>
-    </div>
-    `,
-  }
-  console.log(msg)
-  sgMail
-    .send(msg)
-    .then((response) => {
-      console.log(response[0].statusCode)
-      console.log(response[0].headers)
-    })
-    .catch((error) => {
-      console.error(error)
-    })
+    text: `<div>
+    //   <strong>Hello ${name} Change your password</strong>
+    //   <strong>CLick on change passwod to redirect to the change password page!</strong>
+    //   <a href="${process.env.API_URL}/user/verifytoken/${token}">Change password</a>
+    //   </div>
+    //   `
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
 }
