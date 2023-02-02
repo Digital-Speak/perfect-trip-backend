c4ed454166825f011435a31fc326cf902df0f2e9
nabilcambiaso <nabil.cambiaso@gmail.com>
2023-02-02 13:37:51 +0100

    Changed forgot password design

--- tools/mails/forgotPasswordMail.js
+++ tools/mails/forgotPasswordMail.js
@@ -1,17 +1,7 @@
 require("dotenv").config();
-const sgMail = require('@sendgrid/mail')
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-
 var nodemailer = require('nodemailer');
 
 module.exports = async function SendEmail(email, name, token) {
-  console.log({
-    service: 'gmail',
-    auth: {
-      user: process.env.MAINEMAIL,
-      pass: process.env.MAINEMAILPASS
-    }
-  });
   var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -24,12 +14,13 @@ module.exports = async function SendEmail(email, name, token) {
     from: process.env.MAINEMAIL,
     to: email,
     subject: 'Forgot password',
-    text: `<div>
-    //   <strong>Hello ${name} Change your password</strong>
-    //   <strong>CLick on change passwod to redirect to the change password page!</strong>
-    //   <a href="${process.env.API_URL}/user/verifytoken/${token}">Change password</a>
-    //   </div>
-    //   `
+    html: `<div>
+       <h1 style="background-color: orange">Perfect trip</h1>
+       <strong>Hello ${name}, if you are the one who sent this request click on the link below, else just ignore this email</strong>
+       <strong>CLick on change password to be redirected to the change password page!</strong>
+       <a href="${process.env.API_URL}/user/verifytoken/${token}">Change password</a>
+      </div>
+       `
   };
   
   transporter.sendMail(mailOptions, function(error, info){
